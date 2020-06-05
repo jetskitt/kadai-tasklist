965a3ec0ee96325a86c0e15ef745e5f093f25c78
toshiki.toyama <ttinthebuilding@gmail.com>
2020-06-05 10:27:20 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'tasks#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :tasks
   
